ff6da0a0ca482032bd839e3a7b859e2c3c9ad5a5
Khaled Albadi <Khalid.albadi@maqta.ae>
2023-10-09 08:55:41 +0400

    postlist update

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -5,8 +5,11 @@ import { Directive,ElementRef,HostBinding,HostListener } from '@angular/core'
 })
 export class DropdownDirective {
   @HostBinding('class.open') isOpen = false;
+  @HostBinding('class.show') isShow = false;
   @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
     this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
+    //find the dropdown-menu class in the event target and toggle the isShow property
+    this.isShow = this.elRef.nativeElement.querySelector('.dropdown-menu').contains(event.target) ? !this.isShow : false;
   }
   constructor(private elRef: ElementRef) {}
 }
